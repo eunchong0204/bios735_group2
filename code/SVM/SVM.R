30b6fe43818d9114fb3e44d6a7e25a72853d8078
Wanting Jin <55896013+Wanting-Jin@users.noreply.github.com>
2022-04-23 22:54:09 -0400

    Update SVM.R

--- code/SVM/SVM.R
+++ code/SVM/SVM.R
@@ -6,7 +6,7 @@
 library(tidyverse)
 library(optimx)
 library(caret)
-
+library(pROC)
 
 # set a randomization seed
 set.seed(735)
@@ -43,6 +43,7 @@ heart.data$SleepTime <- as.numeric(heart.data$SleepTime)
 trainIndex <- createDataPartition(heart.data$HeartDisease, p = .8, list = FALSE, times = 1)
 # train set
 heart.train <- heart.data[trainIndex,]
+
 # test set
 heart.test <- heart.data[-trainIndex,]
 
@@ -53,54 +54,51 @@ table(heart.test$HeartDisease) # 0.08558742 prop of positive cases
 
 
 # functions for SVM algorithm
+SVMIndex <- c(sample(which(heart.train$HeartDisease == "No"), sum(heart.train$HeartDisease == "Yes")), which(heart.train$HeartDisease == "Yes"))
+heart.train <- heart.train[SVMIndex,]
+table(heart.train$HeartDisease)
+
+
 # convert categorica data to dummy variables
 dummies <- dummyVars(~.,data = heart.train[,c(3:5,8:14,16:18)],fullRank = TRUE)
 c2 <-predict(dummies,heart.train[,c(3:5,8:14,16:18)])
 X_train <- as.data.frame(cbind(heart.train[,c(2,6:7,15)],c2))
 Y_train <- heart.train[,1]
-data_train <- as.data.frame(cbind(heart.train[,c(1:2,6:7,15)],c2))
-
+#data_train <- as.data.frame(cbind(heart.train[,c(1:2,6:7,15)],c2))
+table(Y_train)
 
 dummies <- dummyVars(~.,data = heart.test[,c(3:5,8:14,16:18)],fullRank = TRUE)
 c2 <-predict(dummies,heart.test[,c(3:5,8:14,16:18)])
 X_test <- as.data.frame(cbind(heart.test[,c(2,6:7,15)],c2))
 Y_test <- heart.test[,1]
-data_test <- as.data.frame(cbind(heart.test[,c(1:2,6:7,15)],c2))
+#data_test <- as.data.frame(cbind(heart.test[,c(1:2,6:7,15)],c2))
 
 
 ## Using package caret
+
+train_control <- trainControl(method="repeatedcv", number=5,classProbs = TRUE)
+
+
 ### Linear
-fit_LSVM <- train(X_train,Y_train,method = "svmLinear")
+fit_LSVM <- train(X_train,Y_train,
+                  method = "svmLinear", 
+                  trControl = train_control, 
+                  tuneLength = 10,
+                  preProcess = c("center","scale"))
+
 fit_LSVM$results # summary of the model fitting
-#ggplot(fit_LSVM,metrix = "Kappa")
-varImp(fitLSVM) # variable importance
+
+Var <- varImp(fit_LSVM) # variable importance
+plot(varImp(object=fit_LSVM,scale=FALSE),top = 11,   main="Linear SVM - Variable Importance")
+
 
 test_Lpred <- predict(fit_LSVM,newdata = X_test) # prediction for the testing data
 confusionMatrix(table(test_Lpred,Y_test)) # compute sensitivity and specificity using the testing data
 
-### Radial
-fit_RSVM <- train(X_train,Y_train,method = "svmRadial")
-fit_RSVM$results # summary of the model fitting
-#ggplot(fit_RSVM,metrix = "Kappa")
-varImp(fit_RSVM) # variable importance
-
-test_Rpred <- predict(fit_RSVM,newdata = X_test) # prediction for the testing data
-confusionMatrix(table(test_Rpred,Y_test)) # compute sensitivity and specificity using the testing data
+save(fit_LSVM,file = "fit_LSVM.Rdata")
 
 
-
-
-
-## method 2, Using package e1071, function svm
-library(e1071)
-gammalist <- c(0.005,0.01,0.015,0.02,0.025,0.03,0.035,0.04,0.045,0.05)
-gammalist <- c(0.01,0.02,0.03,0.04,0.05)
-tune.out <- tune.svm( as.factor(HeartDisease) ~., data = data_train[1:1000,], 
-                      kernel='radial', cost=2^(-1:5), gamma = gammalist)
-summary(tune.out)
-summary(tune.out$best.model)
-svm1 <- predict(tune.out$best.model, data_test[,-1])
-confusionMatrix(svm1, as.factor(data_test$HeartDisease))
-
+prob_SVM <- predict(fit_LSVM, X_test,type = "prob") # ROC curve
+roc_SVM <- roc(heart.test$HeartDisease,prob_SVM$Yes, plot = TRUE, print.auc = TRUE)
 
 
